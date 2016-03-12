4f1ccb89d531ef6a0f89da3f1358b89539ab5e43
ChalkPE <chalkpe@gmail.com>
2016-03-12 23:58:17 +0900

    Updated to ES6

--- js/stream.js
+++ js/stream.js
@@ -7,6 +7,7 @@ Stream.prototype = {
     get: function(i){
         var item = this.items[i];
         if(item === undefined) return;
+
         for(var w = 0; w < this.works.length; w++){
             var result = this.works[w](item);
             if(result === undefined) return;
@@ -17,22 +18,21 @@ Stream.prototype = {
     forEach: function(f){
         for(var i = 0; i < this.items.length; i++){
             var item = this.get(i);
-            if(item === undefined) continue;
-            if(f(item) === true) break;
+            if(item !== undefined && f(item) === true) break;
         }
     },
     toArray: function(){
         var array = [];
-        this.forEach(function(item){ array.push(item); });
+        this.forEach(item => array.push(item));
         return array;
     },
     count: function(){
         var count = 0;
-        this.forEach(function(item){ count++; });
+        this.forEach(item => count++);
         return count;
     },
     filter: function(f){
-        this.works.push(function(item){ if(f(item)) return item; });
+        this.works.push(item => { if(f(item)) return item; });
         return this;
     },
     map: function(f){
@@ -40,30 +40,36 @@ Stream.prototype = {
         return this;
     },
     peek: function(f){
-        this.map(function(item){ f(item); return item; });
+        this.map(item => { f(item); return item; });
         return this;
     },
     allMatch: function(f){
         var allMatch = true;
-        this.forEach(function(item){
-            if(!f(item)) return !(allMatch = false);
-        });
+        this.forEach(item => { if(!f(item)) return !(allMatch = false); });
         return allMatch;
     },
     anyMatch: function(f){
         var anyMatch = false;
-        this.forEach(function(item){
-            if(f(item)) return anyMatch = true;
-        });
+        this.forEach(item => { if(f(item)) return anyMatch = true; });
         return anyMatch;
     },
     noneMatch: function(f){
         return !this.anyMatch(f);
     },
     findFirst: function(f){
-        var found = null;
-        this.forEach(function(){
-            
-        });
+        var found;
+        this.forEach(item => { if(item === undefined) return; found = item; return true; });
+        return found;
     }
 };
+
+Stream.of = function(...args){
+    return new Stream(args);
+};
+
+Stream.empty = new Stream([]);
+Object.freeze(Stream.empty);
+
+Stream.prototype.some = Stream.prototype.anyMatch;
+Stream.prototype.every = Stream.prototype.allMatch;
+Stream.prototype.findAny = Stream.prototype.findFirst;
